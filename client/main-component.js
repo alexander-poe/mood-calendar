65ec1e957dd9e2a7a0640b1f58fe90b241b0065e
alexpoe <alexpoe22@gmail.com>
2017-02-28 12:41:57 -0800

    Fetch entries (#3)
    * added server code from previous mood calendar repo
    
    * basic first page styles added
    
    * mainPage styling and arrangement added
    
    * add main page to react
    
    * made connection between DB and front end
    
    * server messages render to page
    
    * airbnb style guide added

--- client/main-component.js
+++ client/main-component.js
@@ -1,40 +1,55 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import * as actions from './actions';
+import _ from 'lodash';
 import NewPaula from './new-paula';
 
 class MainComponent extends React.Component {
-	render() {
-		return (
-			<div className="mainPageParentContainer">
-	      <div className="mainPageFilter">
-	      </div>
-	      <div className="mainPageEntryContainer">
-	        <div className="entryContainer">
-	        </div>
-	        <div className="entryContainer">
-	        </div>
-	        <div className="entryContainer">
-	        </div>
-	        <div className="entryContainer">
-	        </div>
-	        <div className="entryContainer">
-	        </div>
-	        <div className="entryContainer">
-	        </div>
-	      </div>
-	      <div className="navigationContainer">
-	        <div className="navigationArrowsContainer">
-	          <i className="fa fa-angle-double-left left" aria-hidden="true"></i>
-	          <i className="fa fa-smile-o" aria-hidden="true"></i>
-	          <i className="fa fa-angle-double-right right" aria-hidden="true"></i>
-	        </div>
-	      </div>
-	    </div>
-	)
-	}
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    this.props.dispatch(actions.fetch_hello());
+  }
+  render() {
+    const entries =	this.props.entries ? this.props.entries : null;
+    const nodes = entries !== [] ? entries.map((entry, index) => {
+      console.log(entry);
+      const mood = entry.mood;
+      const date = entry.date;
+      const text = entry.entry;
+      return (
+        <div className="entryContainer">
+          <h2> { mood } </h2>
+          <h2> { date } </h2>
+          <p> { text } </p>
+        </div>
+      );
+    })
+	  : null;
+
+    return (
+      <div className="mainPageParentContainer">
+        <div className="mainPageFilter" />
+        <div className="mainPageEntryContainer">
+          { nodes }
+        </div>
+        <div className="navigationContainer">
+          <div className="navigationArrowsContainer">
+            <i
+              className="fa fa-angle-double-left left"
+              aria-hidden="true"
+            />
+            <i className="fa fa-smile-o" aria-hidden="true" />
+            <i className="fa fa-angle-double-right right" aria-hidden="true" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default connect((state, props) => ({
-	//Select your state -> props mappings here
-	paula: state.paula,
+  entries: state.entries,
+  paula: state.paula,
 }))(MainComponent);
--- server.js
+++ server.js
@@ -2,214 +2,210 @@ const express = require('express');
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8080;
 const app = express();
-require('dotenv').config()
+require('dotenv').config();
 
 const knex = require('knex')({
-    client: 'pg',
-    connection: process.env.DB_URL,
-})
+  client: 'pg',
+  connection: process.env.DB_URL,
+});
 
-app.use(express.static("build"));
+app.use(express.static('build'));
 
-app.get("/users", (req, res) => {
-    knex('users').select('id', 'username', 'password').then((users) => {
-        res.json({
-            users
-        });
-    })
+app.get('/users', (req, res) => {
+  knex('users').select('id', 'username', 'password').then((users) => {
+    res.json({
+      users,
+    });
+  });
 });
 app.get('/entries', (req, res) => {
-    knex('entries').select('mood', 'date', 'entry').then((entries) => {
-        return res.status(200).json({
-            entries
-        })
-    });
+  knex('entries').select('mood', 'date', 'entry').then(entries => res.status(200).json({
+    entries,
+  }));
 });
 app.get('/entries/u', (req, res) => {
-    const body = req.body;
-    console.log(body);
-    knex('entries').where({
-        user_id: 1
-    }).select('mood', 'date', 'entry').then((entries) => {
-        return res.status(200).json({
-            entries
-        })
-    });
+  const body = req.body;
+  console.log(body);
+  knex('entries').where({
+    user_id: 1,
+  }).select('mood', 'date', 'entry').then(entries => res.status(200).json({
+    entries,
+  }));
 });
 app.post('/users', (req, res) => {
-    const body = req.body;
-    if (!body) {
-        return res.status(400).json({
-            message: 'No request body'
-        })
-    }
+  const body = req.body;
+  if (!body) {
+    return res.status(400).json({
+      message: 'No request body',
+    });
+  }
 
-    if (body.username === " ") {
-        return res.status(422).json({
-            message: 'Missing field: username'
-        })
-    }
+  if (body.username === ' ') {
+    return res.status(422).json({
+      message: 'Missing field: username',
+    });
+  }
 
-    if (body.password === " ") {
-        return res.status(422).json({
-            message: 'Missing field: password'
-        })
-    }
+  if (body.password === ' ') {
+    return res.status(422).json({
+      message: 'Missing field: password',
+    });
+  }
 
-    if (typeof body.username !== 'string') {
-        return res.status(422).json({
-            message: 'Incorrect field type: username'
-        })
-    }
+  if (typeof body.username !== 'string') {
+    return res.status(422).json({
+      message: 'Incorrect field type: username',
+    });
+  }
 
-    if (typeof body.password !== 'string') {
-        return res.status(422).json({
-            message: 'Incorrect field type: password'
-        })
-    }
-    knex.insert({
-        username: body.username,
-        password: body.password
-    }).into('users').then(id => {
-        console.log(id);
-        return res.status(201).json({})
-    }).catch(e => {
-        console.error(e);
-        res.sendStatus(500);
-    })
-})
+  if (typeof body.password !== 'string') {
+    return res.status(422).json({
+      message: 'Incorrect field type: password',
+    });
+  }
+  knex.insert({
+    username: body.username,
+    password: body.password,
+  }).into('users').then((id) => {
+    console.log(id);
+    return res.status(201).json({});
+  }).catch((e) => {
+    console.error(e);
+    res.sendStatus(500);
+  });
+});
 app.post('/entries', (req, res) => {
-    const body = req.body;
-    console.log(body);
-    if (!body) {
-        return res.status(400).json({
-            message: 'No request body'
-        })
-    }
+  const body = req.body;
+  console.log(body);
+  if (!body) {
+    return res.status(400).json({
+      message: 'No request body',
+    });
+  }
 
-    if (body.mood === " ") {
-        return res.status(422).json({
-            message: 'Missing field: mood'
-        })
-    }
+  if (body.mood === ' ') {
+    return res.status(422).json({
+      message: 'Missing field: mood',
+    });
+  }
 
-    if (body.entry === " ") {
-        return res.status(422).json({
-            message: 'Missing field: entry'
-        })
-    }
+  if (body.entry === ' ') {
+    return res.status(422).json({
+      message: 'Missing field: entry',
+    });
+  }
 
-    if (typeof body.entry !== 'string') {
-        return res.status(422).json({
-            message: 'Incorrect field type: entry'
-        })
-    }
-    knex.insert({
-        mood: body.mood,
-        date: new Date(),
-        entry: body.entry,
-        user_id: body.user_id
-    }).into('entries').then(id => {
-        console.log(id);
-        return res.status(201).json({})
-    }).catch(e => {
-        console.error(e);
-        res.sendStatus(500);
-    })
-})
+  if (typeof body.entry !== 'string') {
+    return res.status(422).json({
+      message: 'Incorrect field type: entry',
+    });
+  }
+  knex.insert({
+    mood: body.mood,
+    date: new Date(),
+    entry: body.entry,
+    user_id: body.user_id,
+  }).into('entries').then((id) => {
+    console.log(id);
+    return res.status(201).json({});
+  }).catch((e) => {
+    console.error(e);
+    res.sendStatus(500);
+  });
+});
 app.put('/entries', (req, res) => {
-    const body = req.body;
-    if (!body) {
-        return res.status(400).json({
-            message: 'No request body'
-        })
-    }
-    if (body.mood === " ") {
-        return res.status(422).json({
-            message: 'Missing field: mood'
-        })
-    }
-    if (typeof body.username !== 'string') {
-        return res.status(422).json({
-            message: 'Incorrect field type: mood'
-        })
-    }
-    knex('entries').where({
-        id: 1
-    }).update({
-        mood: body.mood
-    }).then(count => {
-        console.log(count);
-        return res.json({})
-    }).catch(e => {
-        console.error(e);
-        res.sendStatus(500);
-    })
-})
+  const body = req.body;
+  if (!body) {
+    return res.status(400).json({
+      message: 'No request body',
+    });
+  }
+  if (body.mood === ' ') {
+    return res.status(422).json({
+      message: 'Missing field: mood',
+    });
+  }
+  if (typeof body.username !== 'string') {
+    return res.status(422).json({
+      message: 'Incorrect field type: mood',
+    });
+  }
+  knex('entries').where({
+    id: 1,
+  }).update({
+    mood: body.mood,
+  }).then((count) => {
+    console.log(count);
+    return res.json({});
+  }).catch((e) => {
+    console.error(e);
+    res.sendStatus(500);
+  });
+});
 app.put('/users', (req, res) => {
-    const body = req.body;
-    knex('users').where({
-        id: body.id
-    }).update({
-        username: body.username
-    }).then(count => {
-        console.log(count);
-        return res.json({})
-    }).catch(e => {
-        console.error(e);
-        res.sendStatus(500);
-    })
-})
+  const body = req.body;
+  knex('users').where({
+    id: body.id,
+  }).update({
+    username: body.username,
+  }).then((count) => {
+    console.log(count);
+    return res.json({});
+  }).catch((e) => {
+    console.error(e);
+    res.sendStatus(500);
+  });
+});
 app.delete('/users', (req, res) => {
-    const body = req.body;
-    if (!body) {
-        return res.status(400).json({
-            message: 'No request body'
-        })
-    }
-    if (body.id === " ") {
-        return res.status(422).json({
-            message: 'Missing field: ID'
-        })
-    }
-    if (typeof body.id === isNaN) {
-        return res.status(422).json({
-            message: 'Incorrect field type: #'
-        })
-    }
-    knex('users').where({
-        id: body.id
-    }).del().then(count => {
-        console.log(count);
-        return res.json({})
-    }).catch(e => {
-        console.error(e);
-        res.sendStatus(500);
-    })
-})
+  const body = req.body;
+  if (!body) {
+    return res.status(400).json({
+      message: 'No request body',
+    });
+  }
+  if (body.id === ' ') {
+    return res.status(422).json({
+      message: 'Missing field: ID',
+    });
+  }
+  if (typeof body.id === isNaN) {
+    return res.status(422).json({
+      message: 'Incorrect field type: #',
+    });
+  }
+  knex('users').where({
+    id: body.id,
+  }).del().then((count) => {
+    console.log(count);
+    return res.json({});
+  }).catch((e) => {
+    console.error(e);
+    res.sendStatus(500);
+  });
+});
 app.delete('/entries', (req, res) => {
-    const body = req.body;
-    if (body.id === null) {
-        return res.status(404).json({
-            message: 'entry not found'
-        })
-        knex('entries').where({
-            id: body.id
-        }).del().then(count => {
-            console.log(count);
-            return res.status(200).json({})
-        }).catch(e => {
-            console.error(e);
-            res.sendStatus(500);
-        })
-    }
-})
+  const body = req.body;
+  if (body.id === null) {
+    return res.status(404).json({
+      message: 'entry not found',
+    });
+    knex('entries').where({
+      id: body.id,
+    }).del().then((count) => {
+      console.log(count);
+      return res.status(200).json({});
+    }).catch((e) => {
+      console.error(e);
+      res.sendStatus(500);
+    });
+  }
+});
 
 function run_server() {
-    app.listen(PORT, HOST, err => {
-        if (err) return console.error(err);
-        console.log(`Listening on port ${PORT}`);
-    });
+  app.listen(PORT, HOST, (err) => {
+    if (err) return console.error(err);
+    console.log(`Listening on port ${PORT}`);
+  });
 }
 
 if (require.main === module) run_server();
