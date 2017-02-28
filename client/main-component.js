ccdcf854ea7d2dd823ec083e852c20d9285daeca
Alexander Poe <alexpoe@Alexanders-MacBook-Air.local>
2017-02-27 19:40:05 -0800

    add main page to react

--- client/main-component.js
+++ client/main-component.js
@@ -4,11 +4,33 @@ import NewPaula from './new-paula';
 
 class MainComponent extends React.Component {
 	render() {
-		return <div>
-			<h1>React/Redux demo</h1>
-			<p>This is where your content goes. Paula is {this.props.paula}.</p>
-			<NewPaula />
-		</div>;
+		return (
+			<div className="mainPageParentContainer">
+	      <div className="mainPageFilter">
+	      </div>
+	      <div className="mainPageEntryContainer">
+	        <div className="entryContainer">
+	        </div>
+	        <div className="entryContainer">
+	        </div>
+	        <div className="entryContainer">
+	        </div>
+	        <div className="entryContainer">
+	        </div>
+	        <div className="entryContainer">
+	        </div>
+	        <div className="entryContainer">
+	        </div>
+	      </div>
+	      <div className="navigationContainer">
+	        <div className="navigationArrowsContainer">
+	          <i className="fa fa-angle-double-left left" aria-hidden="true"></i>
+	          <i className="fa fa-smile-o" aria-hidden="true"></i>
+	          <i className="fa fa-angle-double-right right" aria-hidden="true"></i>
+	        </div>
+	      </div>
+	    </div>
+	)
 	}
 }
 
